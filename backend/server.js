a41bb12c197c0a45057cacbeb94c4bbb03978b45
saifmoh7 <saif.2017.moh@gmail.com>
2022-06-21 23:56:39 +0300

    add redux to pruductpage

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ app.get('/api/products', (req, res) => {
     res.send(data.products)
 })
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id)
+    if (product) {
+        res.send(data.products)
+    } else {
+        res.status(404).send({ message: "Product Not Found" })
+    }
+})
+
 app.listen(port, () => {
     console.log(`Server at http://localhost:${port}`)
 })
