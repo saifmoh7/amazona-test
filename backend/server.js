0a8ea08ca63c86bde6e26799b279b306be91e07c
saifmoh7 <saif.2017.moh@gmail.com>
2022-06-23 22:54:34 +0300

    add cart screen

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,14 @@ app.get('/api/products', (req, res) => {
 })
 
 app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id)
+    const product = data.products.find((x) => x._id === req.params.id);
     if (product) {
-        res.send(data.products)
+        res.send(product)
     } else {
-        res.status(404).send({ message: "Product Not Found" })
+        res.status(404).send({message: "Product Not Found!"});
     }
 })
 
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`)
+    console.log(`Server at http://loctalhost:${port}`)
 })
